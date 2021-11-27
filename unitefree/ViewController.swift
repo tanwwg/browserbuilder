59a4e9164a36e0379b19b32127b42fa3c16085e8
Tan Thor Jen <tthorjen@gmail.com>
2021-11-27 09:11:44 +0800

    Initial Commit

---
+++ unitefree/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  unitefree
+//
+//  Created by Tan Thor Jen on 11/27/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
